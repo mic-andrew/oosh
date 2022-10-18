a6e929108224c05c50322a5ec5f8e71258c6a53d
AyodejiSheen <aoluwasegun009@gmail.com>
2022-10-18 23:32:15 +0100

    hjkhlj

--- src/views/settings/settings.js
+++ src/views/settings/settings.js
@@ -35,7 +35,7 @@ export const Settings = () => {
                         <div className="">
                             <button
                                 onClick={() => handleChange("tab1")}
-                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-gray-700 border-gray-300 dark:text-gray-400 dark:hover:text lg:-gray-300 lg:w-full active"
+                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-orange-600 border-gray-300 dark:text-gray-400 dark:hover:text lg:-gray-300 lg:w-full active"
                                 active={active === "tab1"}
                             >
 
@@ -49,9 +49,9 @@ export const Settings = () => {
                         <div>
                             <button
                                 onClick={() => handleChange("tab2")}
-                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-gray-700 border-gray-300 dark:text-gray-400 dark:hover:text lg:-gray-300 lg:w-full active"
+                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-orange-600 border-gray-300 dark:text-gray-400 dark:hover:text lg:-gray-300 lg:w-full active"
                             >
-                                <i class="fa-solid fa-language mr-3"></i>
+                                <i class="fa-solid fa-language lg:mr-3"></i>
                                 <span className=" hidden lg:inline-flex">Language</span>
                             </button>
                         </div>
@@ -59,7 +59,7 @@ export const Settings = () => {
                         <div>
                             <button
                                 onClick={() => handleChange("tab3")}
-                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-gray-700 border-gray-300 dark:text-gray-400 dark:hover:text lg:-gray-300 lg:w-full active"
+                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-orange-600 border-gray-300 dark:text-gray-400 dark:hover:text lg:-gray-300 lg:w-full active"
                             >
                                 <i class="fa-solid fa-ban lg:mr-3"></i>
                                 <span className=" hidden lg:inline-flex">Blocking</span>
@@ -69,7 +69,7 @@ export const Settings = () => {
                         <div>
                             <button
                                 onClick={() => handleChange("tab4")}
-                                className="lg:text-center p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto xl:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-gray-700 border-gray-300 dark:text-gray-400 dark:hover:textlg:-gray-300 lg:w-full active"
+                                className="lg:text-center p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto xl:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-orange-600 border-gray-300 dark:text-gray-400 dark:hover:textlg:-gray-300 lg:w-full active"
                             >
                                 <i class="fa-regular fa-bell lg:mr-3"></i>
                                 <span className=" hidden lg:inline-flex ">
@@ -81,7 +81,7 @@ export const Settings = () => {
                         <div>
                             <button
                                 onClick={() => handleChange("tab5")}
-                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-gray-700 border-gray-300 dark:text-gray-400 dark:hover:textlg:-gray-300 lg:w-full active"
+                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-orange-600 border-gray-300 dark:text-gray-400 dark:hover:textlg:-gray-300 lg:w-full active"
                             >
                                 <i class="fa-solid fa-lock lg:mr-3"></i>
                                 <span className=" hidden lg:inline-flex">Password & Security</span>
@@ -91,7 +91,7 @@ export const Settings = () => {
                         <div>
                             <button
                                 onClick={() => handleChange("tab6")}
-                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-gray-700 border-gray-300 dark:text-gray-400 dark:hover:textlg:-gray-300 lg:w-full active"
+                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-orange-600 border-gray-300 dark:text-gray-400 dark:hover:textlg:-gray-300 lg:w-full active"
                             >
                                 <i class="fa-solid fa-clock-rotate-left lg:mr-3"></i>
                                 <span className=" hidden lg:inline-flex">Activity Log</span>
@@ -101,7 +101,7 @@ export const Settings = () => {
                         <div>
                             <button
                                 onClick={() => handleChange("tab7")}
-                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-gray-700 border-gray-300 dark:text-gray-400 dark:hover:textlg:-gray-300 lg:w-full active"
+                                className="p-4 md:px-5 md:py-4 lg:px-6 lg:py-5 xl:p-6 text-sm font-medium md:w-28 text-center mx-auto lg:text-left text-gray-500 md:border-x md:border-y lg:border-r-2 border-transparent hover:text-orange-600 border-gray-300 dark:text-gray-400 dark:hover:textlg:-gray-300 lg:w-full active"
                             >
                                 <i class="fa-solid fa-eye lg:mr-3"></i>
                                 <span className=" hidden lg:inline-flex">Viewing & Sharing</span>
